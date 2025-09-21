6fd9d62671e30b15a1ab543ee6174c35d99a21ec
MBronko <michal.brono@protonmail.com>
2025-09-21 13:55:25 +0200

    set firefox about:config

---
+++ roles/firefox-config/files/user.js
@@ -0,0 +1,8 @@
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("general.autoScroll", true);
+user_pref("middlemouse.paste", false);
+user_pref("mousebutton.4th.enabled", false);
+user_pref("mousebutton.5th.enabled", false);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
